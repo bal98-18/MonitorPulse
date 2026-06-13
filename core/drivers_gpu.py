23133d0a8a3ebde45957d6f489338c252dab76d6
bal98-18 <adonis.espana18@gmail.com>
2026-06-12 23:37:11 -0400

    Fixed

--- core/drivers_gpu.py
+++ core/drivers_gpu.py
@@ -49,11 +49,22 @@ class AmdDriver(BaseGPUDriver):
         self.computer.Open()
         
         self.gpu_hw = None
+        
+        # 1. Primero buscamos explícitamente una GPU AMD (Dedicada o bien detectada)
         for hw in self.computer.Hardware:
-            if hw.HardwareType == Hardware.HardwareType.GpuAmd or hw.HardwareType == Hardware.HardwareType.Cpu:
+            if hw.HardwareType == Hardware.HardwareType.GpuAmd:
                 self.gpu_hw = hw
                 self.nombre = hw.Name
                 break
+                
+        # 2. Si no encontró una GPU, buscamos una APU (Gráficos integrados en el CPU)
+        if not self.gpu_hw:
+            for hw in self.computer.Hardware:
+                # Nos aseguramos de que sea un CPU y que sea de la marca AMD
+                if hw.HardwareType == Hardware.HardwareType.Cpu and "AMD" in hw.Name:
+                    self.gpu_hw = hw
+                    self.nombre = f"{hw.Name} (Integrated Graphics)"
+                    break
         
         if not self.gpu_hw:
             raise Exception("No se detectó GPU AMD")
